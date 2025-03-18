ec805621c56df4ded9a22cec01cd65257598d984
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-03-18 09:16:23 +0000

    Add home app with index view, templates, and URL configuration

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth', # required for allauth
     'allauth.account', # required for allauth
     'allauth.socialaccount', # required for allauth
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,10 @@ ROOT_URLCONF = 'milestone_4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
