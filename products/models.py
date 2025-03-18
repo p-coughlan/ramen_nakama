cfa01bfdf28d352208e7dff9d682cea7d95c1c51
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-03-18 16:41:29 +0000

    Add Product and Category models with admin registration

--- products/models.py
+++ products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """
+    A model for the product categories
+    """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """ Returns the name of the category """
+        return self.name
+    
+    def get_friendly_name(self):
+        """ Returns the friendly name of the category """
+        return self.friendly_name
+    
+class Product(models.Model):
+    """
+    A model for the products
+    null = True allows the field to be optional
+    blank = True allows the field to be optional in forms
+    on_delete=models.SET_NULL sets the value of the foreign key to NULL if the associated record in the referenced table is deleted
+    """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """ Returns the name of the product """
+        return self.name
