736a7e8e4e928385bfab6f3ec5f99acb72ac2006
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-03-19 13:35:29 +0000

    Add product detail view and update navigation links for product pages

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'), # This is a path that takes a product_id as an argument
 ]
