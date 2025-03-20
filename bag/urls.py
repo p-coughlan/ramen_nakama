bc0aab642c595717ee56be8fcb934692e680a84a
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-03-20 13:12:12 +0000

    Add functionality to add items to the shopping bag with quantity input

--- bag/urls.py
+++ bag/urls.py
@@ -3,4 +3,5 @@ from . import views # import views from the current directory
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
