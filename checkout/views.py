cf54b32d114f51384f789320a561aaf6a225dcac
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-03-24 11:47:14 +0000

    Add Stripe payment handling and environment configuration
    - Implement real-time validation for Stripe card element
    - Update checkout view to calculate total for Stripe
    - Add Stripe keys to settings and load from environment
    - Modify .gitignore to include env.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -8,11 +13,15 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
     
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51R68DGJuaTYD6cypQ8j4hfp0FwVDNXxG8ou4k2PrdBECPmGZLVwyK3IhCR7HqrWkmSVIqBxokwk49k7XYpBXcF6Q00ZVef5sD9',
+        'stripe_public_key': 'pk_test_51LsAVSDbrSAzYrzcRSLdMAqeaXZkUNLmV0cXhl70xgfdimMu6ZPgf6orKSjM3KQR2JfEfxPjISBRhUV6xXX5WnyA00ZksvPBi2',
         'client_secret': 'test client secret',
     }
 
