e98137ca0feb640f5852a422177ec3d409d29259
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-03-27 11:48:12 +0000

    Add product addition: implement add_product view and template, update URLs

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'), # This is a path that takes a product_id as an argument
+    path('<int:product_id>/', views.product_detail, name='product_detail'), # This is a path that takes a product_id as an argument
+    path('add/', views.add_product, name='add_product'),
 ]
